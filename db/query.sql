012f7ff1f712bdd95b010b2918e97095b034adf1
Traspo <nyaspo8@gmail.com>
2024-01-24 21:45:06 -0500

    added some test code as notes

--- db/query.sql
+++ db/query.sql
@@ -23,3 +23,16 @@ VALUES ("${first_name}", "${last_name}", ${role_id}, ${manager_id});`
 `UPDATE sqlemployees_db.employee
 SET role_id = ${role_id}
 WHERE id = ${emp_id};`
+
+-- Testing multi-joins from Jared Elliott -- NOT ACTULLY USED
+`SELECT e.id, e.first_name AS 'First Name', e.last_name AS 'Last Name', r.title AS 'Title', d.name AS 'Department', r.salary AS 'Salary'
+FROM employee as e 
+INNER JOIN role as r ON r.id = e.id INNER JOIN department as d ON d.id = r.department_id;`
+
+`SELECT e.id AS 'Emp. ID', e.first_name AS 'First Name', e.last_name AS 'Last Name', r.title AS 'Title', d.name AS 'Department', r.salary AS 'Salary', m.first_name AS 'Manager Name'
+FROM employee as e 
+INNER JOIN role as r ON r.id = e.id INNER JOIN department as d ON d.id = r.department_id INNER JOIN employee as m ON e.manager_id = m.id`
+
+`SELECT e.id AS 'Emp. ID', e.first_name AS 'First Name', e.last_name AS 'Last Name', r.title AS 'Title', d.name AS 'Department', r.salary AS 'Salary', GROUP_CONCAT(m.first_name, ' ', m.last_name) AS 'Manager Name' 
+FROM employee as e 
+INNER JOIN role as r ON r.id = e.id INNER JOIN department as d ON d.id = r.department_id INNER JOIN employee as m ON e.manager_id = m.id GROUP BY e.id`
